faa66c469dcea89439eeb5a094c742d8492996a7
anand <anandvaibhaw000@gmail.com>
2025-01-30 23:17:50 +0530

    configured type=module and added cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import Portfolio from './pages/Portfolio';
-import Strategies from './pages/Strategies';
-import MarketUpdates from './pages/MarketUpdates';
+// import Portfolio from './pages/Portfolio';
+// import Strategies from './pages/Strategies';
+// import MarketUpdates from './pages/MarketUpdates';
 import Navbar from './components/Navbar';
 import './index.css';
 
@@ -14,9 +14,9 @@ function App() {
         <div className="container mx-auto p-4">
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/strategies" element={<Strategies />} />
-            <Route path="/market-updates" element={<MarketUpdates />} />
+            {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+            {/* <Route path="/strategies" element={<Strategies />} /> */}
+            {/* <Route path="/market-updates" element={<MarketUpdates />} /> */}
           </Routes>
         </div>
       </div>
