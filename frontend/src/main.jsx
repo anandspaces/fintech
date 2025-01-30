79914ab7eee8c1659db4e6b54dec1c0afec2f81e
anand <anandvaibhaw000@gmail.com>
2025-01-30 23:08:04 +0530

    built src files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { PortfolioProvider } from './context/PortfolioContext';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PortfolioProvider>
+      <App />
+    </PortfolioProvider>
   </StrictMode>,
 )
